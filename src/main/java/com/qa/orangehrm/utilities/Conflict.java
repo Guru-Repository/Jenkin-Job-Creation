c9d02e474c82d65ea5ba47a0095436bcaecc4ee6
guru.amtex@gmail.com <guru.amtex@gmail.com>
2025-12-22 09:40:27 +0530

    First commit

---
+++ src/main/java/com/qa/orangehrm/utilities/Conflict.java
@@ -0,0 +1,10 @@
+package com.qa.orangehrm.utilities;
+
+public class Conflict {
+	public static void testConflict(){
+	    int a = 10;
+	    int b = 20;
+	    int c = a+b;
+	    System.out.println(c);
+	  }
+}
---
+++ src/main/java/com/qa/orangehrm/utilities/Testing.java
@@ -0,0 +1,7 @@
+package com.qa.orangehrm.utilities;
+
+public class Testing {
+public static void main(String[] args) {
+	System.out.println("Tag and Release");
+}
+}
